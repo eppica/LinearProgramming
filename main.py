e90afbdc58c31ea989cb401a4f32504f849f3621
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-03-02 19:06:29 -0300

    Implements Big M
    Closes #6

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import os
 from tabulate import tabulate
 
+M = 999
 
 COLOR = {
     "PURPLE": '\033[95m',
@@ -16,26 +17,35 @@ COLOR = {
     "END": '\033[0m'
 }
 
-base_variables = ['x1', 'x2', 'f1', 'f2', 'a1', 'a2', 'a3']
-non_base_variables = ['a1', 'a2', 'a3']
+base_variables = ['x1', 'x2', 'f1', 'f2', 'f3', 'a1']
+non_base_variables = ['f1', 'f2', 'a1']
 
 matrix = [
-    [1.0, 6.0, -1.0, 0.0, 1.0, 0.0, 0.0],
-    [4.0, 3.0, 0.0, -1.0, 0.0, 1.0, 0.0],
-    [1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+    [2.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+    [0.0, 4.0, 0.0, 1.0, 0.0, 0.0],
+    [6.0, 4.0, 0.0, 0.0, -1.0, 1.0]
 ]
 
-objective = [-15, -32, 0, 0, 0, 0, 0]
-
-objective_aux = [-6, -11, 1, 1, 0, 0, 0]
+objective = [-6, -10, 0, 0, 0, M]
 
 total = 0
 
-total_aux = -37
-
-independent_terms = [7, 12, 18]
+independent_terms = [8, 24, 36]
 
 quotients = []
+
+
+def big_m(function):
+    global total
+    div = -1 * M
+
+    for i, base in enumerate(non_base_variables):
+        if 'a' in base:
+
+            for j, value in enumerate(objective):
+                objective[j] = round(value + div * matrix[i][j], 5)
+
+            total = round(total + div * independent_terms[i], 5)
 
 
 def is_optimum_solution(function):
@@ -61,7 +71,6 @@ def get_pivot_line_index(j):
 
 
 def min_positive(iterable):
-    iterable_aux = iterable.copy()
     for i, number in enumerate(iterable):
         if number <= 0:
             iterable[i] = math.inf
@@ -87,7 +96,7 @@ def reset_pivot_line():
 
 
 def scale_matrix():
-    global total, total_aux
+    global total
     for i, line in enumerate(matrix):
         if line[pivot_column_index] != 0 and i != pivot_line_index:
             div = -1 * line[pivot_column_index]
@@ -101,48 +110,17 @@ def scale_matrix():
         objective[j] = round(value + div * matrix[pivot_line_index][j], 5)
 
     total = round(total + div * independent_terms[pivot_line_index], 5)
-    if not is_optimum_solution(objective_aux):
-        div = -1 * objective_aux[pivot_column_index]
-
-        for j, value in enumerate(objective_aux):
-            objective_aux[j] = round(value + div * matrix[pivot_line_index][j], 5)
-
-        total_aux = round(total_aux + div * independent_terms[pivot_line_index], 5)
-
-
-def two_phase():
-    remove_index = []
-    for i, value in enumerate(base_variables):
-        if 'a' in value:
-            remove_index.append(i)
-
-    for value in sorted(remove_index, reverse=True):
-        base_variables.pop(value)
-        objective.pop(value)
-
-    for i, line in enumerate(matrix):
-        for value in sorted(remove_index, reverse=True):
-            matrix[i].pop(value)
 
 
 def print_matrix(column=None, line=None):
-    if not is_optimum_solution(objective_aux):
-        lines = len(independent_terms) + 3
-        columns = len(base_variables) + 2
-        matrix_aux = lines * [columns * ['']]
-        matrix_aux[0] = organize_line('\\', base_variables, 'b')
-        for i in range(lines - 3):
-            matrix_aux[i + 1] = organize_line(non_base_variables[i], matrix[i], independent_terms[i])
-        matrix_aux[lines - 2] = organize_line('Z', objective, total)
-        matrix_aux[lines - 1] = organize_line('Z\'', objective_aux, total_aux)
-    else:
-        lines = len(independent_terms) + 2
-        columns = len(base_variables) + 2
-        matrix_aux = lines * [columns * ['']]
-        matrix_aux[0] = organize_line('\\', base_variables, 'b')
-        for i in range(lines - 2):
-            matrix_aux[i + 1] = organize_line(non_base_variables[i], matrix[i], independent_terms[i])
-        matrix_aux[lines - 1] = organize_line('Z', objective, total)
+
+    lines = len(independent_terms) + 2
+    columns = len(base_variables) + 2
+    matrix_aux = lines * [columns * ['']]
+    matrix_aux[0] = organize_line('\\', base_variables, 'b')
+    for i in range(lines - 2):
+        matrix_aux[i + 1] = organize_line(non_base_variables[i], matrix[i], independent_terms[i])
+    matrix_aux[lines - 1] = organize_line('Z', objective, total)
 
     if column is None and line is None:
         print(tabulate(matrix_aux, headers='firstrow', tablefmt='fancy_grid'))
@@ -192,17 +170,15 @@ print(' Initial Table\n')
 print_matrix()
 print('\n======================================================================================================================\n')
 
+big_m(objective)
+
 while True:
 
-    if not is_optimum_solution(objective_aux):
-        pivot_column_index = get_pivot_column_index(objective_aux)
-    elif not is_optimum_solution(objective):
+    if not is_optimum_solution(objective):
         pivot_column_index = get_pivot_column_index(objective)
 
     print("Pivot Column Index", pivot_column_index)
     pivot_line_index = get_pivot_line_index(pivot_column_index)
-    if pivot_line_index == -1:
-        continue
     print("Pivot Line Index", pivot_line_index)
     pivot_number = matrix[pivot_line_index][pivot_column_index]
     print("Pivot Number", pivot_number)
@@ -210,17 +186,12 @@ while True:
     print_matrix(pivot_column_index, pivot_line_index)
     reset_pivot_line()
 
-    if not is_optimum_solution(objective_aux):
-        scale_matrix()
-    elif not is_optimum_solution(objective):
+    if not is_optimum_solution(objective):
         scale_matrix()
     else:
         break
 
     quotients.clear()
-
-    if is_optimum_solution(objective_aux):
-        two_phase()
 
 
 print('\n\n\n======================================================================================================================\n')
